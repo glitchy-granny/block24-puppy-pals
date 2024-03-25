c128c7503b67b96eab5ffaed92f0afbb38a33265
Pamela Daniels <pamela.d.daniels@gmail.com>
2024-03-25 17:51:46 -0400

    latest updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import { useState } from "react";
+import { puppyList } from "./data";
+import "./index.css";
 
 function App() {
-  return;
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPup] = useState(null);
+
+  function handleClick(id) {
+    setFeatPup(id);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log(featuredPup);
+
+  return (
+    <div className="container">
+      <div className="puppies-list">
+        {puppies.map((puppy) => (
+          <p key={puppy.id} onClick={() => handleClick(puppy.id)}>
+            {puppy.name}
+          </p>
+        ))}
+      </div>
+
+      {featPupId && (
+        <div className="featured-pup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+          <div className="tricks-list">
+            <h3>Tricks:</h3>
+            {featuredPup.tricks.map((trick) => (
+              <div key={trick.id} className="trick-item">
+                {trick.title}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
